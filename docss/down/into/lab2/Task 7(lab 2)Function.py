6cb419db956b481a058cd12251c8d46a12364830
glynzr <97050713+glynzr@users.noreply.github.com>
2022-03-01 00:53:36 +0400

    Delete docss/down/into/lab2 directory

--- docss/down/into/lab2/Task 7(lab 2)Function.py
+++
@@ -1,8 +0,0 @@
-#Function 7(lab 2):
-x,y=map(float,input("Enter (x,y) koordinates: ").split(","))
-if (-1<=x<=0 and 0<=y<=1) or (y>=x and x**2+y**2<=1 and x>=0) or (x<=0 and x**2+y**2<=1 and y>=x):
-    print("Verilen oblasta daxildi")
-else:
-    print("Verilen oblasta daxil deyil")
-    
-    
